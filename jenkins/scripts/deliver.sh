e45d4c2182e8f501f6829848f63ff40b9b43b293
YaeSakura <jiangkaiwen0911@gmail.com>
2024-04-16 14:45:32 +0800

    Update deliver.sh

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -3,7 +3,6 @@
 mvn jar:jar install:install help:evaluate -Dexpression=project.name
 
 NAME=`mvn -q -DforceStdout help:evaluate -Dexpression=project.name`
-
 VERSION=`mvn -q -DforceStdout help:evaluate -Dexpression=project.version`
 
 echo "Name: ${NAME}"
